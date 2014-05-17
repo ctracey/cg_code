1d773915f5e10ad280d17bb3a1bd462a426d02d7
Chris Tracey <tracey.chris@gmail.com>
2014-05-17 22:08:28 +1000

    command accepts order path as argument

--- lib/flower_shop/cli.rb
+++ lib/flower_shop/cli.rb
@@ -3,8 +3,11 @@ require 'clamp'
 module FlowerShop
   class CLI < Clamp::Command
 
+    parameter "ORDER_PATH", "Path to order file"
+
     def execute
       puts "running Flower Shop Invoicer"
     end
+
   end
 end
--- spec/bin/flower_shop_invoicer_spec.rb
+++ spec/bin/flower_shop_invoicer_spec.rb
@@ -3,10 +3,25 @@ require 'spec_helper'
 describe "flower shop invoicer" do
 
   let(:command) { "./bin/flower_shop_invoicer" }
+  let(:order_path) { "./spec/fixtures/order.txt" }
 
   it "runs and logs to the console" do
-    console_output = `#{command}`
+    console_output = `#{command} #{order_path}`
     console_output.include?("running Flower Shop Invoicer").should be_true
+  end
+
+  context "order file parameter" do
+    let(:hide_output) { " > /dev/null" }
+
+    it "accepts the path to an order file as order parameter" do
+      system("#{command} #{order_path} #{hide_output}").should be_true
+    end
+
+    context "no parameter provided" do
+      it "the exit code is 1" do
+        system(command).should be_false
+      end
+    end
   end
 
 end
