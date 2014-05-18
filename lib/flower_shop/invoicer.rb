e9e90e932b616f36c69c12ab68c8c3737b23a4e4
Chris Tracey <tracey.chris@gmail.com>
2014-05-18 20:45:26 +1000

    order item calculate bundles

--- lib/flower_shop/invoicer.rb
+++ lib/flower_shop/invoicer.rb
@@ -1,17 +1,64 @@
+require 'yaml'
+
 module FlowerShop
   class Invoicer
+
+    BUNDLES_CONFIG = "config/bundles.yml"
+
     def initialize(order)
       @order = order
+      @bundle_config = load_bundle_config
     end
 
     def invoice
       invoice = Invoice.new
+
       @order.items.each do |item|
         invoice_item = InvoiceItem.new(item)
+        invoice_item.bundles = calculate_bundles(item)
         invoice.add_item(invoice_item)
       end
 
       return invoice
+    end
+
+    private
+
+    def load_bundle_config
+      yaml = File.read(Invoicer::BUNDLES_CONFIG)
+      YAML.load(yaml)["bundles"]
+    end
+
+    def calculate_bundles(order_item)
+      bundles = []
+      quantity = order_item.quantity
+
+      sizes = @bundle_config[order_item.code].keys
+      sizes.sort!.reverse!
+
+      largest = sizes.first
+      while quantity > largest do
+        bundles << largest
+        quantity -= largest
+      end
+
+      #smaller than smallest bundle
+      if quantity < sizes.last
+        bundles << sizes.last
+        return bundles
+      end
+
+      sizes.each_with_index do |size, index|
+        if quantity == size
+          bundles << size
+          break
+        elsif quantity > size
+          bundles << sizes[index-1]
+          break
+        end
+      end
+
+      bundles
     end
 
   end
--- spec/lib/invoicer_spec.rb
+++ spec/lib/invoicer_spec.rb
@@ -2,17 +2,64 @@ require 'spec_helper'
 
 describe "FlowerShop::Invoicer" do
 
+  let(:order_item) { double(FlowerShop::OrderItem, quantity: 10, code: "R12") }
+  let(:order_items) { [order_item] }
+  let(:order)       { double(FlowerShop::Order, items: order_items ) }
+
+  subject { FlowerShop::Invoicer.new(order) }
+
   describe "#invoice" do
-    it "returns an invoice representing all the order items" do
-      order_item1 = double(FlowerShop::OrderItem, quantity: 1, code: "R11")
-      order_item2 = double(FlowerShop::OrderItem, quantity: 2, code: "R12")
-      order_items = [order_item1, order_item2]
-      order = double(FlowerShop::Order, items: order_items )
+    context "multiple items" do
+      let(:order_item2) { double(FlowerShop::OrderItem, quantity: 2, code: "R12") }
+      let(:order_items) { [order_item, order_item2] }
 
+      it "returns an invoice representing all the order items" do
+        invoice = subject.invoice
+        expect(invoice.items.size).to eq 2
+      end
+    end
+
+    it "calculates the required bundles for each invoice item" do
+      invoice_item = FlowerShop::InvoiceItem.new(order_item)
+      allow(FlowerShop::InvoiceItem).to receive(:new) { invoice_item }
+
+      invoice = subject.invoice
+
+      expect(invoice_item.bundles).to eq [10]
+    end
+  end
+
+  describe "#load_bundle_config" do
+    it "returns a hash representing the config file" do
+      order = double(FlowerShop::Order)
       invoicer = FlowerShop::Invoicer.new(order)
-      invoice = invoicer.invoice
 
-      expect(invoice.items.size).to eq 2
+      bundles = invoicer.send(:load_bundle_config)
+
+      expect(bundles.keys).to include("R12")
+      expect(bundles.keys).to include("L09")
+      expect(bundles.keys).to include("T58")
+    end
+  end
+
+  describe "#calculate_bundles" do
+    scenarios = [
+      {quantity: 10, code: "R12", expected_bundles: [10],                    desc: "matching bundle size"},
+      {quantity: 5,  code: "R12", expected_bundles: [5],                     desc: "matching bundle size"},
+      {quantity: 7,  code: "R12", expected_bundles: [10],                    desc: "inbetween bundle sizes"},
+      {quantity: 3,  code: "R12", expected_bundles: [5],                     desc: "smaller than smallest bundle"},
+      {quantity: 10, code: "L09", expected_bundles: [9, 3],                  desc: "bigger than largest bundle"},
+      {quantity: 20, code: "R12", expected_bundles: [10, 10],                desc: "is double the largest bundle"},
+      {quantity: 23, code: "R12", expected_bundles: [10, 10, 5],             desc: "more than double the largest bundle"},
+      {quantity: 51, code: "R12", expected_bundles: [10, 10, 10, 10, 10, 5], desc: "thats massive"},
+    ]
+
+    scenarios.each do |scenario|
+      it "returns correct bundles for order item #{scenario[:desc]}" do
+        order_item = FlowerShop::OrderItem.parse("#{scenario[:quantity]} #{scenario[:code]}")
+        bundles = subject.send(:calculate_bundles, order_item)
+        expect(bundles).to eq scenario[:expected_bundles]
+      end
     end
   end
 
