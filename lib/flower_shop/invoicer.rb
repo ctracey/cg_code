388fedb1ff79858dc23800c9db8ddf3eb0e9e3c3
Chris Tracey <tracey.chris@gmail.com>
2014-05-19 12:15:14 +1000

    refactoring

--- lib/flower_shop/invoicer.rb
+++ lib/flower_shop/invoicer.rb
@@ -29,28 +29,28 @@ module FlowerShop
 
       product_bundles = @bundle_config.product_config(order_item.code)
       item_bundles = Bundles.new(product_bundles)
-      bundle_sizes = product_bundles.keys
+      descending_bundle_sizes = product_bundles.keys
 
       #larger than largest bundle
-      largest_bundle = bundle_sizes.first
+      largest_bundle = descending_bundle_sizes.first
       while quantity > largest_bundle do
         item_bundles.add_bundle(largest_bundle)
         quantity -= largest_bundle
       end
 
       #smaller than smallest bundle
-      if quantity < bundle_sizes.last
-        item_bundles.add_bundle(bundle_sizes.last)
+      if quantity < descending_bundle_sizes.last
+        item_bundles.add_bundle(descending_bundle_sizes.last)
         return item_bundles
       end
 
       #within our bundle sizes
-      bundle_sizes.each_with_index do |size, index|
+      descending_bundle_sizes.each_with_index do |size, index|
         if quantity == size
           item_bundles.add_bundle(size)
           break
         elsif quantity > size
-          item_bundles.add_bundle(bundle_sizes[index-1])
+          item_bundles.add_bundle(descending_bundle_sizes[index-1])
           break
         end
       end
